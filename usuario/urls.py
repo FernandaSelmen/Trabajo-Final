0c6087362d677f27fdcde12907ce1fb3855af0d3
Fernanda <fernandaselmen@gmail.com>
2023-04-04 15:37:21 -0300

    modificaciones del CRUD

--- usuario/urls.py
+++ usuario/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', inicio, name = 'inicio'),
     path('publicaciones/', publicaciones, name = 'publicaciones'),
     path('mostrar/', mostrar, name = 'mostrar'),
-    path('editar/<int:id_texto>/', agregar, name = 'editar'),
+    path('editar/<int:id_texto>/', editar, name = 'editar'),
     path('eliminar/<int:id_texto>/', eliminar, name = 'eliminar'),
-    path('agregar/<int:id_texto>/', editar, name = 'agregar'),
+    path('agregar/', agregar, name = 'agregar'),
       ]
 
